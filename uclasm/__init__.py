023b1634c6df55551008b9e4034c73bed59a0a27
qinyichen <qinyichen@chenqinyis-MacBook-Pro.local>
2019-05-09 16:54:51 -0700

    Fixed Jacob's dumb mistakes, ignore .DS_Store

--- uclasm/__init__.py
+++ uclasm/__init__.py
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
 from .utils import *
 from .filters import *
 from .counting import *
-=======
-from .utils.data_structures import Template, World
-from .filters.stats_filter import stats_filter
-from .filters.topology_filter import topology_filter
-from .filters.permutation_filter import permutation_filter
-from .filters.neighborhood_filter import neighborhood_filter
-from .filters.elimination_filter import elimination_filter
-from .filters.all_filters import all_filters
-from .counting.isomorphisms import count_isomorphisms
->>>>>>> neighborhood_filter
