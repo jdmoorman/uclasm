e6497079fb5931728da8986c8b9801f96f1d4330
Jacob Moorman <jdmoorman@Jacobs-MacBook-Pro.local>
2019-05-10 17:10:47 -0700

    features becomes features_list in stats_filter

--- uclasm/filters/stats_filter.py
+++ uclasm/filters/stats_filter.py
@@ -8,7 +8,8 @@ def compute_features(graph, channels=None):
     """
     is_cand_any is a bool vector of nodes we care about
     """
-    features = []
+
+    features_list = []
 
     # default to computing features for every channel in the default order
     if channels is None:
@@ -19,30 +20,30 @@ def compute_features(graph, channels=None):
         adj = graph.ch_to_adj[channel]
 
         # in degree
-        features.append(adj.sum(axis=0).A)
+        features_list.append(adj.sum(axis=0).A)
 
         # out degree
-        features.append(adj.sum(axis=1).T.A)
+        features_list.append(adj.sum(axis=1).T.A)
 
         # max in edge multiplicity
-        features.append(adj.max(axis=0).A)
+        features_list.append(adj.max(axis=0).A)
 
         # max out edge multiplicity
-        features.append(adj.max(axis=1).todense().T.A)
+        features_list.append(adj.max(axis=1).todense().T.A)
 
         # neighbors coming in
-        features.append((adj > 0).sum(axis=0).A)
+        features_list.append((adj > 0).sum(axis=0).A)
 
         # neighbors going out
-        features.append((adj > 0).sum(axis=1).T.A)
+        features_list.append((adj > 0).sum(axis=1).T.A)
 
         # self edges
-        features.append(np.reshape(adj.diagonal(), (1, -1)))
+        features_list.append(np.reshape(adj.diagonal(), (1, -1)))
 
         # reciprocated edges
-        features.append(adj.multiply(adj.T).sum(axis=0).A)
+        features_list.append(adj.multiply(adj.T).sum(axis=0).A)
 
-    return np.concatenate(features, axis=0)
+    return np.concatenate(features_list, axis=0)
 
 def stats_filter(tmplt, world, candidates, *, verbose=False, **kwargs):
     tmplt_feats = compute_features(tmplt)
