44d98329ebf364f8b634205592f64b17e61ae778
domyang <domyang@ucla.edu>
2020-02-25 11:25:35 -0800

    Mergining in my optimizations for the equivalence code

--- uclasm/equivalence/equivalence_data_structure.py
+++ uclasm/equivalence/equivalence_data_structure.py
@@ -1,147 +1,143 @@
-""" Implementation of an equivalence data structure for partitioning
-	vertices into equivalence classes. From Pattis' ICS46 lecture's note:
-	https://www.ics.uci.edu/~pattis/ICS-46/lectures/notes/equivalence.txt
-	by TimNg
-	Last update: 7/8/19 (basic implementation)"""
+''' Implementation of an equivalence data structure for partitioning
+    vertices into equivalence classes. From Pattis' ICS46 lecture's note:
+    https://www.ics.uci.edu/~pattis/ICS-46/lectures/notes/equivalence.txt
+    by TimNg
+    Last update: 7/8/19 (basic implementation)'''
 
 from collections import defaultdict
+from copy import deepcopy
 
+class Equivalence:
+    ''' Equivalence class data structure for efficient equivalence classes
+    computation'''
+    def __init__(self, starting_set = set()):
+        ''' starting_set is a set of node to start with '''
+        # dictionary of parents where each key is mapped to its parent
+        # start with everything maps to itself
+        self.parent_map = {a: a for a in starting_set}
+        # dictionary of root: size_of_tree
+        # also serves as something to store the roots
+        self.root_size_map = {a : 1 for a in starting_set}
 
-class Equivalence(object):
-	""" Equivalence class data structure for efficient equivalence classes
-	computation"""
-	def __init__(self, starting_set):
-		""" starting_set is a set of node to start with """
-		# dictionary of parents where each key is mapped to its parent
-		# start with everything maps to itself
-		self.parent_map = {a: a for a in starting_set}
-		# dictionary of root: size_of_tree
-		# also serves as something to store the roots
-		self.root_size_map = {a: 1 for a in starting_set}
+    ### METHODS ###
+    def add_singleton(self, new_value):
+        ''' add a new value as its own equiv class'''
+        assert new_value not in self.parent_map, "Equivalence.add_singleton: "+str(new_value) + " is already in the equiv class!"
+        self.parent_map[new_value] = new_value
+        self.root_size_map[new_value] = 1
 
-	# ## METHODS ###
-	def add_singleton(self, new_value):
-		""" add a new value as its own equiv class"""
-		assert new_value not in self.parent_map, "Equivalence.add_singleton: "+str(new_value) + " is already in the equiv class!"
-		self.parent_map[new_value] = new_value
-		self.root_size_map[new_value] = 1
+    def merge_classes_of(self, a , b):
+        ''' merge the equivalence classes of a and b together
+        by setting each parent's map to point to the same root '''
+        assert a in self.parent_map, "Equivalence.merge_classes_of: Value " + str(a) + " does not exist."
+        assert b in self.parent_map, "Equivalence.merge_classes_of: Value " + str(b) + " does not exist."
+        # get the roots while compressing the tree
+        root_of_a = self.compress_to_root(a)
+        root_of_b = self.compress_to_root(b)
+        if (root_of_a == root_of_b):
+            return # they are already in the same equivalence class
+        # find the "big" root and the "small" root
+        small_root, big_root = (root_of_a, root_of_b) if self.root_size_map[root_of_a] < self.root_size_map[root_of_b] \
+                     else (root_of_b, root_of_a)
+        # now we change the root of the smaller size map to the bigger one
+        # then we update the size of the new equiv class and then remove the smaller one
+        # as a root
+        self.parent_map[small_root] = big_root
+        self.root_size_map[big_root] += self.root_size_map[small_root] # we grew!
+        del self.root_size_map[small_root]  # remove the small_root from the roots dict
 
-	def merge_classes_of(self, a , b):
-		""" merge the equivalence classes of a and b together
-		by setting each parent's map to point to the same root """
-		assert a in self.parent_map, "Equivalence.merge_classes_of: Value " + str(a) + " does not exist."
-		assert b in self.parent_map, "Equivalence.merge_classes_of: Value " + str(b) + " does not exist."
-		# get the roots while compressing the tree
-		root_of_a = self.compress_to_root(a)
-		root_of_b = self.compress_to_root(b)
-		if root_of_a == root_of_b:
-			return # they are already in the same equivalence class
-		# find the "big" root and the "small" root
-		small_root, big_root = (root_of_a, root_of_b) if self.root_size_map[root_of_a] < self.root_size_map[root_of_b] \
-					 else (root_of_b, root_of_a)
-		# now we change the root of the smaller size map to the bigger one
-		# then we update the size of the new equiv class and then remove the smaller one
-		# as a root
-		self.parent_map[small_root] = big_root
-		self.root_size_map[big_root] += self.root_size_map[small_root] # we grew!
-		del self.root_size_map[small_root]  # remove the small_root from the roots dict
+    def merge_set(self, set_to_merge):
+        ''' given a set of elements in Equivalence, merge them together '''
+        for a in set_to_merge:
+            assert a in self.parent_map, "Equivalence.merge_set: Value " + str(a) + " does not exist."
+        one_elem = next(iter(set_to_merge))
+        for a in set_to_merge: # more efficient way is to merge pairwise???
+            self.merge_classes_of(one_elem, a)
 
-	def merge_set(self, set_to_merge):
-		""" given a set of elements in Equivalence, merge them together """
-		for a in set_to_merge:
-			assert a in self.parent_map, "Equivalence.merge_set: Value " + str(a) + " does not exist."
-		one_elem = next(iter(set_to_merge))
-		for a in set_to_merge: # more efficient way is to merge pairwise???
-			self.merge_classes_of(one_elem, a)
+    def partition(self, equivalence_relation: 'function', *args, **kwargs) -> None :
+        ''' given an equivalence_relation function compute the appropriate equiv classes
+        of the elements in the Equivalence data structure. Users have to make sure of semantic
+        and care should be taken to make sure the equivalence_relation passed is an actual equiv relation, otherwise
+        the output will be undefined.
+        --> Checking in_same_class(a,b) will be the same as using equivalence_relation(a,b)
+        This uses a divide and conquer approach on the root_size_map dictionary.
+        The time complexity should be n*logn (need a proof though...)
+            for a divide and conquer approach ... now it's just naive implementation '''
+        classes = []
+        for i in self.root_size_map:
+            found = False
+            for c in classes:
+                # if these two classes are equivalence then we add them
+                if equivalence_relation(next(iter(c)), i, *args, *kwargs):
+                    c.add(i)
+                    found = True
+                    break
+            if not found: # new class
+                classes.append(set([i]))
+        for c in classes:
+            self.merge_set(c)
+        return classes
 
-	def partition(self, equivalence_relation: 'function', *args, **kwargs) -> [set]:
-		""" given an equivalence_relation function compute the appropriate equiv classes
-		of the elements in the Equivalence data structure. Users have to make sure of semantic
-		and care should be taken to make sure the equivalence_relation passed is an actual equiv relation, otherwise
-		the output will be undefined.
-		--> Checking in_same_class(a,b) will be the same as using equivalence_relation(a,b)
-		This uses a divide and conquer approach on the root_size_map dictionary.
-		The time complexity should be n*logn (need a proof though...)
-			for a divide and conquer approach ... now it's just naive implementation """
-		classes = []
-		# TODO: parallelize
-		for i in self.root_size_map:
-			found = False
-			for c in classes:
-				# if these two classes are equivalence then we add them
-				if equivalence_relation(next(iter(c)), i, *args, *kwargs):
-					c.add(i)
-					found = True
-					break
-			if not found:  # new class
-				classes.append({i})
-		for c in classes:
-			self.merge_set(c)
-		return classes
+    ### QUERIES ###
+    def in_same_class(self, a, b) -> bool:
+        ''' return a bool indicating if a and b are in the same classes '''
+        assert a in self.parent_map, "Equivalence.in_same_class: " + str(a) + " does not exist."
+        assert b in self.parent_map, "Equivalence.in_same_class: " + str(b) + " does not exist."
+        root_of_a = self.compress_to_root(a)
+        root_of_b = self.compress_to_root(b)
+        return root_of_a == root_of_b
 
-	# ## QUERIES ###
-	def in_same_class(self, a, b) -> bool:
-		""" return a bool indicating if a and b are in the same classes """
-		assert a in self.parent_map, "Equivalence.in_same_class: " + str(a) + " does not exist."
-		assert b in self.parent_map, "Equivalence.in_same_class: " + str(b) + " does not exist."
-		root_of_a = self.compress_to_root(a)
-		root_of_b = self.compress_to_root(b)
-		return root_of_a == root_of_b
+    def __len__(self):
+        ''' returns the number of elements in the equiv class'''
+        return len(self.parent_map)
 
-	def __len__(self):
-		""" returns the number of elements in the equiv class"""
-		return len(self.parent_map)
+    def class_count(self):
+        ''' return the number of unique equiv classes'''
+        return len(self.root_size_map)
 
-	def class_count(self):
-		""" return the number of unique equiv classes"""
-		return len(self.root_size_map)
+    def classes(self) -> [{'equiv classes'}]:
+        ''' returns a list of sets of equivalence classes (python doesn't allow set of sets)'''
+        answer_map = defaultdict(set) # lookup defaultdict if not sure. just instantiate an empty set by default
+        for i in self.parent_map:
+            root_of_i = self.compress_to_root(i)
+            answer_map[root_of_i].add(i)
+        return answer_map
 
-	def classes(self) -> [{'equiv classes'}]:
-		""" returns a list of sets of equivalence classes (python doesn't allow set of sets)"""
-		answer_map = defaultdict(set) # lookup defaultdict if not sure. just instantiate an empty set by default
-		for i in self.parent_map:
-			root_of_i = self.compress_to_root(i)
-			answer_map[root_of_i].add(i)
-		return list(answer_map.values())
+    def non_trivial_classes(self) -> [{'equiv classes'}]:
+        ''' returns a list of sets of non-trivial (>1) equivalence classes\
+         (python doesn't allow set of sets)'''
+        answer_map = defaultdict(set) # lookup defaultdict if not sure. just instantiate an empty set by default
+        for i in self.parent_map:
+            root_of_i = self.compress_to_root(i)
+            answer_map[root_of_i].add(i)
+        return [i for i in answer_map.values() if len(i)>1]
 
-	def non_trivial_classes(self) -> [{int}]:
-		""" returns a list of sets of non-trivial (>1) equivalence classes\
-		(python doesn't allow set of sets)"""
-		answer_map = defaultdict(set) # lookup defaultdict if not sure. just instantiate an empty set by default
-		for i in self.parent_map:
-			root_of_i = self.compress_to_root(i)
-			answer_map[root_of_i].add(i)
-		return [i for i in answer_map.values() if len(i)>1]
+    def __str__(self):
+        ''' print out the equiv classes '''
+        return "Equiv Classes: " + str(self.classes()) + "\nParent_map: " + str(self.parent_map) + "\nRoot_size_map: "+str(self.root_size_map)
 
-	def __str__(self):
-		""" print out the equiv classes """
-		return "Equiv Classes: " + str(self.classes()) + "\nParent_map: " + str(self.parent_map)\
-			+ "\nRoot_size_map: " + str(self.root_size_map)
+    ### PRIVATE METHODS ###
+    def compress_to_root(self, a) -> 'root of a':
+        ''' returns the root of a, and on the way, set the roots of the parents of a
+        to be the root of a'''
+        assert a in self.parent_map, "Equivalence.compress_to_root: "+ str(a) + "does not exist."
+        parents_of_a = set() # set to store the parents of a
+        curr_val = a   # current value
+        while curr_val != self.parent_map[curr_val]:
+            # while the current value is not its own parent (otherwise it's the root)
+            parents_of_a.add(curr_val) #  we add it to the set of parents_of_a
+            curr_val = self.parent_map[curr_val] # we keep going to the next parent
+        # note that curr_val is now the root: the condition to break
+        # now we set all the parents of a to the root, this is the compression step
+        for i in parents_of_a:
+            self.parent_map[i] = curr_val
+        return curr_val
 
-	def __repr__(self):
-		result = f"#classes/#vertices={self.class_count()}/{len(self)}. " \
-			f"Non-triv classes size: {[len(i) for i in self.non_trivial_classes()]}"
-		return result
+    def rep(self, a):
+        reutrn self.compress_to_root(a)
 
-	def get_equiv_size(self, node):
-		return self.root_size_map[self.compress_to_root(node)]
-
-	def compress_to_root(self, a) -> 'root of a':
-		""" returns the root of a, and on the way, set the roots of the parents of a
-		to be the root of a"""
-		assert a in self.parent_map, "Equivalence.compress_to_root: "+ str(a) + "does not exist."
-		parents_of_a = set()  # set to store the parents of a
-		curr_val = a   # current value
-		while curr_val != self.parent_map[curr_val]:
-			# while the current value is not its own parent (otherwise it's the root)
-			parents_of_a.add(curr_val)  # we add it to the set of parents_of_a
-			curr_val = self.parent_map[curr_val] # we keep going to the next parent
-		# note that curr_val is now the root: the condition to break
-		# now we set all the parents of a to the root, this is the compression step
-		for i in parents_of_a:
-			self.parent_map[i] = curr_val
-		return curr_val
-
-	def get_all_roots(self) -> {int or str}:
-		""" Return all the roots of the equiv classes"""
-		return set(self.root_size_map.keys())
+    def copy(self):
+        """
+        Return a deep copy of self
+        """
+        return deepcopy(self)
--- uclasm/equivalence/partition_sparse.py
+++ uclasm/equivalence/partition_sparse.py
@@ -4,29 +4,32 @@
     by TimNg
     Last Update: 7/8/19 (Basic Functionalities)'''
 
-from equivalence_data_structure import Equivalence
+from .equivalence_data_structure import Equivalence
 import numpy as np
+from queue import Queue
 
 def array_equal(mat1, mat2):
     ''' special array_equal for sparse matrix'''
     return (mat1 != mat2).nnz==0
 
-def permutation_relation(vertex_a, vertex_b, adj_matrix: 'sparse matrix'):
+def permutation_relation(vertex_a, vertex_b, adj_matrix: 'sparse matrix',
+        adj_matrix_csc: 'sparse matrix', row_nnz, col_nnz):
     ''' 
     We compare two vertices: a and b to see if they are equivalence
     under permutation. This can be done by checking if certain pieces
-    of their values in the adj_matrix are the same (see notes)'''
+    of their values in the adj_matrix are the same (see notes)
+    '''
     n = adj_matrix.shape[0] # get the shape
-    assert vertex_a in range(0, n), \
-        "permutation_relation: invalid index for vertex " + str(vertex_a)
-    assert vertex_b in range(0, n), \
-        "permutation_relation: invalid index for vertex " + str(vertex_b)
     # handle the trivial case
     if vertex_a == vertex_b:
         return True
     # in the paper x < y so we use similar notation
     x,y = (vertex_a,vertex_b) if vertex_a < vertex_b else (vertex_b, vertex_a)
+    if col_nnz[x] == 0 and col_nnz[y] == 0 and row_nnz[x] == 0 and row_nnz[x] == 0:
+        return True
     # several conditions to check
+    if row_nnz[x] != row_nnz[y] or col_nnz[x] != col_nnz[y]:
+        return False
     if adj_matrix[x,x] != adj_matrix[y,y]:
         return False
     if adj_matrix[x,y] != adj_matrix[y,x]:
@@ -37,9 +40,9 @@ def permutation_relation(vertex_a, vertex_b, adj_matrix: 'sparse matrix'):
             array_equal(adj_matrix[x,y+1:n], adj_matrix[y,y+1:n])):
         return False
     # check the columns
-    if not (array_equal(adj_matrix[0:x,x],   adj_matrix[0:x,y]) and \
-            array_equal(adj_matrix[x+1:y,x], adj_matrix[x+1:y,y]) and \
-            array_equal(adj_matrix[y+1:n,x], adj_matrix[y+1:n,y])):
+    if not (array_equal(adj_matrix_csc[0:x,x],   adj_matrix_csc[0:x,y]) and \
+            array_equal(adj_matrix_csc[x+1:y,x], adj_matrix_csc[x+1:y,y]) and \
+            array_equal(adj_matrix_csc[y+1:n,x], adj_matrix_csc[y+1:n,y])):
         return False
     # we have passed all the test if we reach here... return True now
     return True
@@ -51,7 +54,126 @@ def partition_vertices(adj_matrix: '2d numpy array') \
     assert adj_matrix.shape[0] == adj_matrix.shape[1], "partition_vertices: Must be square matrix!"
     # initialize an equivalence relation fill with nodes
     nodes = range(0, adj_matrix.shape[0])
+    adj_matrix_csc = adj_matrix.tocsc()
+    row_nnz = adj_matrix.getnnz(1)
+    col_nnz = adj_matrix_csc.getnnz(0)
     vertices_partition = Equivalence(nodes)
     # partition into equivalence classes
-    vertices_partition.partition(permutation_relation, adj_matrix)
+    vertices_partition.partition(permutation_relation, adj_matrix, adj_matrix_csc,
+                                 row_nnz, col_nnz)
     return vertices_partition
+
+
+def equivalent(u, v, adj_mat_csrs, adj_mat_cscs, row_nnzs, col_nnzs):
+    """
+    Check if vertex u is equivalent to vertex v
+    """
+    if u == v:
+        return True
+
+    # Swap them if v > u
+    u, v = (u, v) if u < v else (v, u)
+
+    # We do preliminary checks to rule out as many equivalences as possible
+    # because the later checks are expensive
+    # We check that the degrees match
+    for channel in adj_mat_csrs:
+        row_nnz = row_nnzs[channel]
+        col_nnz = col_nnzs[channel]
+        if row_nnz[u] != row_nnz[v] or col_nnz[u] != col_nnz[v]:
+            return False
+
+    for channel in adj_mat_csrs:
+        row_nnz = row_nnzs[channel]
+        col_nnz = col_nnzs[channel]
+        if col_nnz[u] == 0 and col_nnz[v] == 0 and row_nnz[u] == 0 and row_nnz[v] == 0:
+            continue
+
+        adj_mat_csr = adj_mat_csrs[channel]
+        adj_mat_csc = adj_mat_cscs[channel]
+        n = adj_mat_csr.shape[0]
+        
+        if adj_mat_csr[u,u] != adj_mat_csr[v,v]:
+            return False
+        if adj_mat_csr[u,v] != adj_mat_csr[v,u]:
+            return False
+
+        # we do this check to avoid checking subarrays if we don't have to
+        if row_nnz[u] != 0:
+            # check the rows (3 pieces) -- maybe there are better ways to do this
+            if not (array_equal(adj_mat_csr[u,0:u],   adj_mat_csr[v,0:u]) and
+                    array_equal(adj_mat_csr[u,u+1:v], adj_mat_csr[v,u+1:v]) and
+                    array_equal(adj_mat_csr[u,v+1:n], adj_mat_csr[v,v+1:n])):
+                return False
+        # check the columns
+        if col_nnz[u] != 0:
+            if not (array_equal(adj_mat_csc[0:u,u],   adj_mat_csc[0:u,v]) and
+                    array_equal(adj_mat_csc[u+1:v,u], adj_mat_csc[u+1:v,v]) and
+                    array_equal(adj_mat_csc[v+1:n,u], adj_mat_csc[v+1:n,v])):
+                return False
+    return True
+
+
+def partition_neighbors(neighbors, adj_mat_csrs, adj_mat_cscs, row_nnzs, col_nnzs):
+    eq_classes = [[neighbors[0]]]
+    for neighbor in neighbors[1:]:
+        for eq_class in eq_classes:
+            if equivalent(neighbor, eq_class[0], adj_mat_csrs, adj_mat_cscs,
+                          row_nnzs, col_nnzs):
+                eq_class.append(neighbor)
+                break
+        else:
+            eq_classes.append([neighbor])
+
+    return eq_classes
+
+
+def bfs_partition_graph(graph):
+    queue = Queue()
+    visited = np.zeros(graph.n_nodes, dtype=np.bool)
+    visited[0] = True
+    all_eq_classes = []
+    # We start at vertex 0
+    queue.put(0)
+
+    adj_mat_csrs = graph.ch_to_adj
+    adj_mat_cscs = {ch: adj_mat.tocsc() for ch, adj_mat in adj_mat_csrs.items()}
+    row_nnzs = {ch: adj_mat.getnnz(1) for ch, adj_mat in adj_mat_csrs.items()}
+    col_nnzs = {ch: adj_mat.getnnz(0) for ch, adj_mat in adj_mat_cscs.items()}
+
+    count = 0
+    while True:
+        if queue.empty():
+            break
+        curr_vertex = queue.get()
+
+        neighbors = graph.sym_composite_adj[curr_vertex].nonzero()[1]
+        neighbors = neighbors[~visited[neighbors]]
+        count += len(neighbors)
+        visited[neighbors] = True
+        if len(neighbors) == 0:
+            continue
+        eq_classes = partition_neighbors(neighbors, adj_mat_csrs, adj_mat_cscs,
+                                         row_nnzs, col_nnzs)
+        all_eq_classes.extend(eq_classes)
+        # For each equivalence class, we put a representative
+        for eq_class in eq_classes:
+            queue.put(eq_class[0])
+    
+    # We still need to find which equivalence class 0 is in
+    for eq_class in all_eq_classes:
+        if equivalent(0, eq_class[0], adj_mat_csrs, adj_mat_cscs, 
+                      row_nnzs, col_nnzs):
+            eq_class.append(0)
+            break
+    else:
+        # 0 is in its own class
+        all_eq_classes.append([0])
+
+    equivalence = Equivalence(list(range(graph.n_nodes)))
+    for eq_class in all_eq_classes:
+        x = eq_class[0]
+        for y in eq_class[1:]:
+            equivalence.merge_classes_of(x, y)
+
+    return equivalence
