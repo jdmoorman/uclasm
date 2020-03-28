857b3a099bf79f28138fc43b630d5265eccb3e87
Jacob Moorman <jdmoorman@math.ucla.edu>
2020-03-28 13:58:39 -0700

    rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -66,3 +66,7 @@ html_favicon = 'favicon.ico'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- Hack from the internet --------------------------------------------------
+
+master_doc = 'index'
