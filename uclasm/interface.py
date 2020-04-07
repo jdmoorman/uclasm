89e2cf74af2cc1f2433d58d3d45817f6fec64f93
Thomas Tu <thomastu@ucla.edu>
2020-04-06 19:08:51 -0700

    add count_isomorphisms function

--- uclasm/interface.py
+++ uclasm/interface.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from .matching import MatchingProblem
 from .matching import local_cost_bound, global_cost_bound
+from .matching import search
 
 # Dummy variable for old code that needs it
 all_filters = None
@@ -52,3 +53,32 @@ def run_filters(tmplt, world, *, candidates=None, filters=None, verbose=False):
             break
         print(smp)
     return tmplt, world, smp.candidates()
+
+def count_isomorphisms(tmplt, world, candidates=None, verbose=False):
+    """Temporary interface for old code. Counts the number of isomorphisms.
+    Likely incredibly inefficient.
+
+    Parameters
+    ----------
+    tmplt : Graph
+        A Graph object
+    world : Graph
+        A Graph object
+    candidates : ndarray(bool)
+        Candidates to initialize with
+    verbose : bool
+        Flag for verbose output
+    """
+    if candidates is None:
+        tmplt, world, candidates = run_filters(tmplt, world, verbose=verbose)
+    fixed_costs = np.zeros(candidates.shape)
+    fixed_costs[~candidates] = float("inf")
+    smp = MatchingProblem(tmplt, world, fixed_costs=fixed_costs)
+    local_cost_bound.nodewise(smp)
+    local_cost_bound.edgewise(smp)
+    global_cost_bound.from_local_bounds(smp)
+
+    iso_list = search.greedy_best_k_matching(smp, k=-1, verbose=verbose)
+
+    import IPython; IPython.embed()
+    return len(iso_list)
