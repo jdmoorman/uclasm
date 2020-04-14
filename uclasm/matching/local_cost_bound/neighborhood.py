aa926dfdafee7a2db1e49655c68535ee30c670f7
Qinyi Chen <qinyichen@g.ucla.edu>
2020-04-13 18:25:59 -0700

    Speed up neighborhood

--- uclasm/matching/local_cost_bound/neighborhood.py
+++ uclasm/matching/local_cost_bound/neighborhood.py
@@ -61,20 +61,21 @@ def neighborhood(smp):
         tmplt_seq = tmplt_edge_seqs[tnode_idx]
         world_seq = world_edge_seqs[wnode_idx]
 
+        # TODO: rewrite this part of neighborhood cost bound computation
+        # to only take into account the matching in the tmplt node nbhd
         lap_mat_rows = []
-        for tnbr_idx in range(smp.tmplt.n_nodes):
+        for tnbr_idx in smp.tmplt.is_nbr[tnode_idx].nonzero()[1]:
             # The world nodes that are not candidates to tnbr_idx are np.Inf
             lap_mat_row = smp.global_costs[tnbr_idx]
             lap_mat_row[lap_mat_row != np.Inf] = 0
             # Check if all of the necessary edges are present
             for i, edge_count in enumerate(tmplt_seq[:,tnbr_idx].A.flat):
                 if edge_count > 0:
-                    lap_mat_row = \
-                        lap_mat_row + np.maximum(edge_count-world_seq[i].A, 0)
-            lap_mat_rows.append(lap_mat_row.flatten())
+                    lap_mat_row += np.maximum(edge_count-world_seq[i].A, 0).flatten()
+            lap_mat_rows.append(lap_mat_row)
 
         lap_mat = np.stack(lap_mat_rows)
         row_idxs, col_idxs = optimize.linear_sum_assignment(lap_mat)
-        # TODO: Figure out the relationship between local_costs and global_costs
-        # for neighborhood-local-costs.
+
+        print ("For node {} there are {} nodes in its nbhd".format(tnode_idx, np.size(row_idxs)))
         smp.local_costs[tnode_idx, wnode_idx] = lap_mat[row_idxs, col_idxs].sum()
