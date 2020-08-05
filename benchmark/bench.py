1928b6d2a9989c5ecc34c48fdc715fa1495bf000
Qinyi Chen <qinyichen@g.ucla.edu>
2020-08-04 17:54:08 -0700

    Compare lad and uclasm

--- benchmark/bench.py
+++ benchmark/bench.py
@@ -1,17 +1,34 @@
 import uclasm
 import uclasm.utils.data_structures as ds
 import networkx as nx
+import time
 
 from run_solvers import *
 
 
 """Note: Solnon benchmarks do not take isolated nodes into account."""
 
+def connected_component_subgraphs(G):
+        for c in nx.connected_components(G):
+            yield G.subgraph(c)
 
-tmplt = ds.from_networkx_graph(nx.erdos_renyi_graph(5, 0.2))
-world = ds.from_networkx_graph(nx.erdos_renyi_graph(10, 0.2))
+tmplt_G = max(connected_component_subgraphs(nx.erdos_renyi_graph(10, 0.3)), key=len)
+world_G = max(connected_component_subgraphs(nx.erdos_renyi_graph(30, 0.2)), key=len)
+
+tmplt = ds.from_networkx_graph(tmplt_G)
+world = ds.from_networkx_graph(world_G)
 
 LAD_output = run_LAD(tmplt, world, enum=True)
-candidates, num_isomorphisms, runtime = parse_LAD_output(tmplt, world, LAD_output)
+print(LAD_output)
+
+candidates_LAD, num_isomorphisms, runtime_LAD = parse_LAD_output(tmplt, world, LAD_output)
+
+start_uclasm = time.time()
+tmplt, world, candidates_uclasm = uclasm.run_filters(tmplt, world)
+runtime_uclasm = time.time()-start_uclasm
+
+assert (np.all(candidates_LAD==candidates_uclasm))
+
+print("LAD takes {}s; UCLASM takes {}s".format(runtime_LAD, runtime_uclasm))
 
 import IPython; IPython.embed()
