d013812d6b61e44ee345f3b3823a9097eb6021b9
EphesosX <thomastu@ucla.edu>
2021-07-16 16:25:12 -0700

    tests for a two triangle SMP requiring world reduction

--- tests/test_search.py
+++ tests/test_search.py
@@ -50,6 +50,26 @@ def smp_noisy():
     smp = MatchingProblem(tmplt, world, global_cost_threshold=1)
     return smp
 
+@pytest.fixture
+def smp_two_triangle():
+    """Template graph is a single edge, world graph is two triangles"""
+    adj = csr_matrix([[0,1],[0,0]])
+    nodelist = pd.DataFrame(['t1','t2'], columns=[Graph.node_col])
+    tmplt = Graph([adj],['c1'], nodelist, None)
+    tmplt.convert_adj_to_edgelist()
+    adj2 = csr_matrix([[0, 1, 0, 0, 0, 0],
+                      [0, 0, 1, 0, 0, 0],
+                      [1, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 1, 0],
+                      [0, 0, 0, 0, 0, 1],
+                      [0, 0, 0, 1, 0, 0]])
+    nodelist = pd.DataFrame(['a','b','c','d','e','f'], columns=[Graph.node_col])
+    edgelist = None
+    world = Graph([adj2],['c1'], nodelist, edgelist)
+    world.convert_adj_to_edgelist()
+    smp = MatchingProblem(tmplt, world, global_cost_threshold=0)
+    return smp
+
 class TestGreedySearch:
     """Tests related to the greedy search """
     def test_greedy_search(self, smp):
@@ -153,3 +173,19 @@ class TestGreedySearchRecursive:
         assert solutions[0].cost == 1
         for i in range(3):
             assert dict(solutions[0].matching)[i] == i
+
+class TestGreedySearchTwoTriangle:
+    def test_greedy_search_noisy_high_thresh_k_5(self, smp_two_triangle):
+        smp_two_triangle.global_cost_threshold = 0.0
+        local_cost_bound.nodewise(smp_two_triangle)
+        local_cost_bound.edgewise(smp_two_triangle)
+        global_cost_bound.from_local_bounds(smp_two_triangle)
+        solutions = search.greedy_best_k_matching_recursive(smp_two_triangle, k=10, verbose=True)
+        assert len(solutions) == 6
+        solution_matchings = []
+        for solution in solutions:
+            assert solution.cost == 0.0
+            solution_matchings.append(solution.matching)
+        for i in range(3):
+            assert ((0,i),(1,(i+1)%3)) in solution_matchings
+            assert ((0,3+i),(1,3+(i+1)%3)) in solution_matchings
